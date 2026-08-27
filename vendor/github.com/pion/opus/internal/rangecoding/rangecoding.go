bb36e92de2fa99a24bd18e399d9460337e4b081c
Riflxz <limbangan144@gmail.com>
2026-08-28 01:03:01 +0700

    Hapus vendor/github.com/pion/opus/internal/rangecoding/rangecoding.go

--- vendor/github.com/pion/opus/internal/rangecoding/rangecoding.go
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
-// SPDX-License-Identifier: MIT
-
-// Package rangecoding provides a Range coder for the Opus bitstream
-package rangecoding
